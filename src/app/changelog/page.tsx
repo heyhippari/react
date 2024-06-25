5ded78ff0ca2813530474b584bcd9720b230db61
MrTimscampi <iamtimscampi@gmail.com>
2024-06-25 16:28:29 +0200

    fix: missing key on changelog page

--- src/app/changelog/page.tsx
+++ src/app/changelog/page.tsx
@@ -14,22 +14,22 @@ export default async function ChangelogPage() {
       <h1 className="line-clamp-2 w-fit text-ellipsis bg-gradient-to-r from-blue-600 to-blue-400 bg-clip-text text-4xl font-bold leading-relaxed text-transparent">
         Changelog
       </h1>
-      {changelogs.map((changelog: any) => (
-        <Accordion type="single" collapsible>
-          <AccordionItem value={changelog._id}>
+      <Accordion type="single" collapsible>
+        {changelogs.map((changelog: any, index: number) => (
+          <AccordionItem key={index} value={changelog._id}>
             <AccordionTrigger>
               <h2 className="text-xl font-bold">{changelog.date}</h2>
             </AccordionTrigger>
             <AccordionContent>
               <ul className="list-inside list-disc">
-                {changelog.changes.map((change: any) => (
-                  <li>{change}</li>
+                {changelog.changes.map((change: any, index: number) => (
+                  <li key={index}>{change}</li>
                 ))}
               </ul>
             </AccordionContent>
           </AccordionItem>
-        </Accordion>
-      ))}
+        ))}
+      </Accordion>
     </div>
   );
 }
