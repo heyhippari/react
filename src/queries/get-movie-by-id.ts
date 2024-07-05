ab81399687a4deeaac74d7e4a5ba8b43b21367dc
MrTimscampi <iamtimscampi@gmail.com>
2024-07-05 14:23:33 +0200

    fix: extra comma in movie query

--- src/queries/get-movie-by-id.ts
+++ src/queries/get-movie-by-id.ts
@@ -26,7 +26,7 @@ export function getMovieById(
                   uuid,
                   type
                 )
-              ),
+              )
           )
         ),
         studio: studios (
