91e884b8dcf32903084afb5cce4c7dd9f2c0be7b
MrTimscampi <iamtimscampi@gmail.com>
2024-08-28 20:11:30 +0200

    build: remove sanity to reduce build size

--- src/utils/sanity/schema.ts
+++
@@ -1,6 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity';
-import changelogType from './types/changelogType';
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [changelogType],
-};
