0041e1b458bc4973e0212cdf859d7827a6952c95
Hippari <iamtimscampi@gmail.com>
2024-10-28 19:53:03 +0100

    fix(middleware): next.js can't read String.raw

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -2,7 +2,7 @@
 
 import { Dialog, DialogContent } from '@/components/ui/dialog';
 import { cn } from '@/core/utils/ui';
-import { type Dialogproperties } from '@radix-ui/react-dialog';
+import { type DialogProps } from '@radix-ui/react-dialog';
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
 import { Command as CommandPrimitive } from 'cmdk';
 import * as React from 'react';
@@ -22,7 +22,7 @@ const Command = React.forwardRef<
 ));
 Command.displayName = CommandPrimitive.displayName;
 
-type CommandDialogProperties = Dialogproperties;
+type CommandDialogProperties = DialogProps;
 
 const CommandDialog = ({
   children,
