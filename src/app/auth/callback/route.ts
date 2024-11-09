c18bdada5ba4201a97b2109d3d030f7e04eaec4c
Hippari <iamtimscampi@gmail.com>
2024-11-09 20:11:29 +0100

    fix(auth): properly handle errors on OAuth callback

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,15 +1,31 @@
 import { userService } from "@/services/user.service";
-import { NextResponse } from "next/server";
+import * as Sentry from "@sentry/nextjs";
+import { NextRequest, NextResponse } from "next/server";
 
 /**
  * Handle the OAuth callback from Supabase.
  * @param request The incoming request.
  * @returns The response to send back to the client.
  */
-export async function GET(request: Request) {
-  const { origin, searchParams } = new URL(request.url);
-  const code = searchParams.get("code");
-  const next = searchParams.get("next") ?? "/";
+export async function GET(request: NextRequest) {
+  // Supabase sends us error messages as query parameters.
+  // Check if we have some before we do anything else.
+  const searchParameters = request.nextUrl.searchParams;
+
+  if (searchParameters.has("error") || searchParameters.has("error_code")) {
+    // Capture the error with Sentry.
+    const error = searchParameters.get("error_description") ??
+      searchParameters.get("error");
+    Sentry.captureException(
+      new Error(error ?? "An unknown error occurred during authentication."),
+    );
+
+    return NextResponse.redirect("/auth/auth-error");
+  }
+
+  const origin = request.nextUrl.origin;
+  const code = searchParameters.get("code");
+  const next = searchParameters.get("next") ?? "/";
 
   if (code) {
     try {
